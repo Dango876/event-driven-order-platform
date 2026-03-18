43ea2a038de40bfc2dead777fb031f164312c79e
Artem Vasin <dangelos876@gmail.com>
2026-03-18 19:58:51 +0300

    ci: fix user-service checkstyle star import

--- services/user-service/src/main/java/com/procurehub/user/api/UserController.java
+++ services/user-service/src/main/java/com/procurehub/user/api/UserController.java
@@ -3,7 +3,10 @@ package com.procurehub.user.api;
 import com.procurehub.user.model.UserProjection;
 import com.procurehub.user.repository.UserProjectionRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
